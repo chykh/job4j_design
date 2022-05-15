22437463308f8ada2f8de284fc9a62e11e323a89
chykh <chykh@mail.ru>
2022-05-15 13:20:54 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539 #300960]

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -5,8 +5,8 @@ import java.util.NoSuchElementException;
 
 public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
-    int row = -1;
-    int column = 0;
+    int j = -1;
+    int i = 0;
 
     public MatrixIt(int[][] data) {
         this.data = data;
@@ -14,27 +14,25 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-         for (int k = column; k < data.length; k++) {
-            if (data[k].length != 0) {
-                return true;
-            }
+
+        if (j == data[i].length - 1 && data[i].length != 0) { //шаг
+            j = -1;
+            i++;
         }
-        return false;
+
+        while (data[i].length == 0 && i < data.length - 1) { //проскок null
+            i++;
+        }
+
+        return !(i == data.length - 1 && j == data[i].length - 1);
+
     }
 
     @Override
     public Integer next() {
         if (hasNext()) {
-            if (row < data[column].length - 1) {
-                row++;
-            } else {
-                row = 0;
-                column++;
-            }
-            while (data[column].length == 0) {
-                column++;
-            }
-            return data[column][row];
+            j++;
+            return data[i][j];
         } else {
             throw new NoSuchElementException();
         }
---
+++ src/test/java/ru/job4j/it/qw.java
@@ -0,0 +1,17 @@
+package ru.job4j.it;
+
+public class qw {
+    public static void main(String[] args) {
+        int i = 0;
+
+        int[][] in = {
+                {}
+        };
+
+        while (in[i].length == 0) { //проскок null
+            i++;
+        }
+       // System.out.println(in.length);
+
+    }
+}
