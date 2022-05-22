906810fc789b9695ff55e1fe20178287607d7a31
chykh <chykh@mail.ru>
2022-05-22 16:30:39 +0300

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -85,5 +85,4 @@ public class SimpleArrayList<T> implements SimpleList<T> {
         };
 
     }
-
 }
--- src/main/java/ru/job4j/collection/SimpleList.java
+++ src/main/java/ru/job4j/collection/SimpleList.java
@@ -1,9 +1,14 @@
 package ru.job4j.collection;
 
 public interface SimpleList<T> extends Iterable<T> {
+
     void add(T value);
+
     T set(int value, T newValue);
+
     T remove(int index);
+
     T get(int index);
+
     int size();
 }
