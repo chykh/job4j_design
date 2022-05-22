e55f350f0cfd922e2a790a40dbb752ac94825316
chykh <chykh@mail.ru>
2022-05-22 16:28:42 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,89 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size > container.length - 1) {
+            container = grow();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, container.length);
+        T rsl = container[index];
+        container[index] = newValue;
+        modCount++;
+        return rsl;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, container.length);
+        T rsl = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return rsl;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, container.length);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private T[] grow() {
+        T[] newContainer = (T[]) new Object[container.length * 2];
+        System.arraycopy(container, 0, newContainer, 0, container.length);
+        return newContainer;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            private final int expectedModCount = modCount;
+            private int step;
+
+            @Override
+            public boolean hasNext() {
+            return step < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[step++];
+            }
+        };
+
+    }
+
+}
