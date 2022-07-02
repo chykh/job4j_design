407fb91131fd6819c80cab28cf6b13ddc5e2e440
chykh <chykh@mail.ru>
2022-07-02 21:15:44 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,40 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        int c = 0;
+
+
+
+        Map<Integer, String> map = new HashMap<>();
+        for (User prev : previous) {
+            map.put(prev.getId(), prev.getName());
+        }
+
+        for (User curr : current) {
+
+            c = curr.getId();
+            if (map.get(c) == null) {
+                added++;
+            }
+
+            if (map.get(c) != null && !curr.getName().equals(map.get(c))) {
+                changed++;
+            }
+
+        }
+
+        deleted = map.size() - current.size() + added;
+
+        return new Info(added, changed, deleted);
+    }
+
+}
