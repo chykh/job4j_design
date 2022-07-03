830d9851566ff758387e8e98a3697b119d24714b
chykh <chykh@mail.ru>
2022-07-03 18:10:07 +0300

    Вспомогательный

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -1,7 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.util.Arrays;
+import java.io.IOException;
 
 public class Matrix {
 
@@ -17,12 +17,14 @@ public class Matrix {
 
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
-            for (int[] array : multiple(6)) {
-                String st = Arrays.toString(array);
-                out.write(st.getBytes());
+            for (int[] array : multiple(3)) {
+                for (int i : array) {
+                    out.write(Integer.toString(i).getBytes());
+                    out.write(" ".getBytes());
+                }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
