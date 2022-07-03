576b3a316f3e01939dc568b68a96f747d9ad3385
chykh <chykh@mail.ru>
2022-07-03 17:29:13 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+public class Matrix {
+
+    public static int[][] multiple(int size) {
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
+            for (int[] array : multiple(5)) {
+                String st = Arrays.toString(array);
+                out.write(st.getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
