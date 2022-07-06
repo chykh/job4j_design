65679263ad4f950330c1b223ca60f05eee1880b6
chykh <chykh@mail.ru>
2022-07-06 19:16:42 +0300

    Вспомогательный

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,13 +1,25 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class ResultFile {
-    public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+/* try (FileOutputStream out = new FileOutputStream("result.txt")) {
             out.write("Hello, world!".getBytes());
             out.write(System.lineSeparator().getBytes());
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+ */
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(new FileOutputStream("result.txt")))) {
+                    out.println("Hello, world!!!");
+                } catch (IOException e) {
             e.printStackTrace();
         }
     }
