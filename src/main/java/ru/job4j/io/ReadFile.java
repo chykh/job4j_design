73ee977ff56873965a632127b196641919a860b1
chykh <chykh@mail.ru>
2022-07-06 17:41:46 +0300

    Вспомогательный

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,24 +1,37 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
+
+/* чтение по байтам:
+
+try (FileInputStream in = new FileInputStream("input.txt")) {
+    StringBuilder text = new StringBuilder();
+    int read;
+    while ((read = in.read()) != -1) {
+        text.append((char) read);
+    }
+    System.out.println(text);
+
+    String[] lines = text.toString().split(System.lineSeparator());
+    for (String line : lines) {
+        System.out.println(line);
+    }
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
+чтение по оберткам:
+ */
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            System.out.println(text);
-
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
-            }
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            in.lines().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
 }
