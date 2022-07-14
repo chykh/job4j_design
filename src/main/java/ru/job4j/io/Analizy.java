7061e54bf84c92b8708867b922e29306086bfbea
chykh <chykh@mail.ru>
2022-07-14 13:13:36 +0300

    2. Анализ доступности сервера. [#859]`

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,42 +7,30 @@ import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-         List<String> result = new ArrayList<>();
 
-         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+         try (BufferedReader in = new BufferedReader(new FileReader(source));
+              PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
              List<String> list = in.lines().collect(Collectors.toList());
-             String res1 = null;
-             String res2;
              boolean flag = false;
 
              for (String string : list) {
                  String[] str = string.split(" ");
 
                  if ((str[0].equals("400") || str[0].equals("500")) && !flag) {
-                    res1 = str[1];
                     flag = true;
+                    out.print(str[1] + ";");
                     continue;
                  }
 
                  if (flag && !str[0].equals("400") && !str[0].equals("500")) {
-                    res2 = res1 + ";" + str[1];
                     flag = false;
-                    result.add(res2);
+                    out.println(str[1]);
                  }
              }
+
          } catch (IOException e) {
              e.printStackTrace();
          }
-
-         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-             for (String string:result) {
-                   out.println(string);
-             }
-
-         } catch (FileNotFoundException e) {
-             e.printStackTrace();
-         }
-
     }
 
     public static void main(String[] args) {
