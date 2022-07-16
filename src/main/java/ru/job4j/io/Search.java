7ee8107396c75d202098b29c1304d743824cf4ab
chykh <chykh@mail.ru>
2022-07-16 18:59:25 +0300

    5. Валидация параметров запуска. [#246865 #322733]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -16,12 +16,18 @@ public class Search {
     }
 
     public static boolean validate(String[] args) {
+        if (args[0].isEmpty() || !args[0].contains(".")) {
+            throw new IllegalArgumentException("wrong first argument");
+        }
+        if (args[1].isEmpty() || args[1].length() > 4) {
+            throw new IllegalArgumentException("wrong second argument");
+        }
         return args.length == 2;
     }
 
     public static void main(String[] args) throws IOException {
         if (!validate(args)) {
-            throw new IllegalArgumentException("You must enter 2 arguments: path and rule");
+            throw new IllegalArgumentException();
         }
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
