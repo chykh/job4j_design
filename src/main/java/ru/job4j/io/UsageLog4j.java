6a0b66db0e2a036e097a33187294f695d6ec1909
chykh <chykh@mail.ru>
2022-08-09 21:29:29 +0300

    Вспомогательный

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,20 +8,11 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Petr Arsentev";
-        int age = 33;
-        LOG.debug("User info name : {}, age : {}", name, age);
-
-        int i = 15;
-        char ch = 'q';
-        byte b = 78;
-        boolean flag = false;
-        double number = 0.133;
-        long l = 1000000L;
-        float f = 0.000000001f;
-        short s = 120;
-        LOG.debug("Primitive types: int = {}, char = {}, byte = {}, boolean = {},"
-                + " double = {}, long = {}, float = {}, short = {}", i, ch, b, flag, number, l, f, s);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 
 }
