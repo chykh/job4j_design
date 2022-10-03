85fcbbd8258be835fb9490798f0e7dee6c6d0a6a
chykh <chykh@mail.ru>
2022-10-03 20:06:15 +0300

    0.1. Связи между таблицами [#373615]

--- sql/husband_wife.sql
+++ sql/husband_wife.sql
@@ -1,30 +1,35 @@
 create table husband(
     id serial primary key,
-	name varchar(255)
+	name varchar(255),
+	marriageCertificate int
  );
 
 create table wife(
     id serial primary key,
-	name varchar(255)
+	name varchar(255),
  );
+ 
+create table wife(
+    id serial primary key,
+	name varchar(255)
+);
 
 create table husband_wife(
     id serial primary key,
     husband_id int references husband(id) unique,
     wife_id int references wife(id) unique,
-	marriageCertificate int
 );
 
-insert into husband(name) values ('Ken');
-insert into husband(name) values ('Ted');
-insert into husband(name) values ('Fred');
+insert into husband(name, marriageCertificate) values ('Ken', 100);
+insert into husband(name, marriageCertificate) values ('Ted', 120);
+insert into husband(name, marriageCertificate) values ('Fred', null);
 
 insert into wife(name) values ('Barby');
 insert into wife(name) values ('Olga');
 insert into wife(name) values ('Nikolay');
 
-insert into husband_wife(husband_id, wife_id, marriageCertificate) values (1, 2 , 10122);
-insert into husband_wife(husband_id, wife_id, marriageCertificate) values (2, 1 , 15135);
-insert into husband_wife(husband_id, wife_id, marriageCertificate) values (3, 3 , null);
+insert into husband_wife(husband_id, wife_id) values (1, 2);
+insert into husband_wife(husband_id, wife_id) values (2, 1);
+insert into husband_wife(husband_id, wife_id) values (3, 3);
 
 select * from wife where id in (select wife_id from husband_wife)
