b307062cc238bc4ee1807061a38a506fe411e89f
chykh <chykh@mail.ru>
2022-10-04 16:03:06 +0300

    0.1. Связи между таблицами [#373615]

--- sql/husband_wife.sql
+++ sql/husband_wife.sql
@@ -1,28 +1,29 @@
 create table husband(
     id serial primary key,
-	name varchar(255),
-	marriageCertificate int
+	name varchar(255)
  );
 
-create table wife(
-    id serial primary key,
-	name varchar(255),
- );
- 
 create table wife(
     id serial primary key,
 	name varchar(255)
-);
+ );
 
-create table husband_wife(
+ create table husband_wife(
     id serial primary key,
     husband_id int references husband(id) unique,
-    wife_id int references wife(id) unique,
-);
+    wife_id int references wife(id) unique
+ );
+ 
+ create table marriageCertificate(
+ 	id serial primary key,
+	number int,
+	husband_id int references husband(id),
+    wife_id int references wife(id)
+ );
 
-insert into husband(name, marriageCertificate) values ('Ken', 100);
-insert into husband(name, marriageCertificate) values ('Ted', 120);
-insert into husband(name, marriageCertificate) values ('Fred', null);
+insert into husband(name) values ('Ken');
+insert into husband(name) values ('Ted');
+insert into husband(name) values ('Fred');
 
 insert into wife(name) values ('Barby');
 insert into wife(name) values ('Olga');
@@ -32,4 +33,10 @@ insert into husband_wife(husband_id, wife_id) values (1, 2);
 insert into husband_wife(husband_id, wife_id) values (2, 1);
 insert into husband_wife(husband_id, wife_id) values (3, 3);
 
-select * from wife where id in (select wife_id from husband_wife)
+select * from wife where id in (select wife_id from husband_wife);
+
+insert into marriageCertificate(number, husband_id, wife_id) values (100, 1, 2);
+insert into marriageCertificate(number, husband_id, wife_id) values (200, 2, 1);
+insert into marriageCertificate(number, husband_id, wife_id) values (null, 3, 3);
+
+select * from marriageCertificate;
