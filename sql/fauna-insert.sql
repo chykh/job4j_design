b59cd966172a8db2bab339dfa2b2d27341f6d5cd
chykh <chykh@mail.ru>
2022-10-06 22:31:06 +0300

    0. Select [#6861]

---
+++ sql/fauna-insert.sql
@@ -0,0 +1,9 @@
+insert into fauna(name, avg_age, discovery_date) values ('cat', 5000, '200-01-01');
+insert into fauna(name, avg_age, discovery_date) values ('dog', 4000, '300-01-01');
+insert into fauna(name, avg_age, discovery_date) values ('blackfish', 300, '1800-11-01');
+insert into fauna(name, avg_age, discovery_date) values ('elephant', 12000, '100-03-30');
+insert into fauna(name, avg_age, discovery_date) values ('eagle', 15000, '1600-11-21');
+insert into fauna(name, avg_age, discovery_date) values ('tiger', 2000, null);
+insert into fauna(name, avg_age, discovery_date) values ('butterfly', 500, '1990-06-01');
+insert into fauna(name, avg_age, discovery_date) values ('crocodile', 51000, null);
+insert into fauna(name, avg_age, discovery_date) values ('fishfrog', 10, '1999-01-01');
