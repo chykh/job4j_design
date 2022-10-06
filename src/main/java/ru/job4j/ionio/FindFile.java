be9db6f4436e48665ffa0c200619db093b5554b2
chykh <chykh@mail.ru>
2022-10-06 21:11:40 +0300

    2. Поиск файлов по критерию [#783 #344191]

--- src/main/java/ru/job4j/ionio/FindFile.java
+++ src/main/java/ru/job4j/ionio/FindFile.java
@@ -1,5 +1,7 @@
 package ru.job4j.ionio;
 
+import ru.job4j.io.ArgsName;
+import ru.job4j.io.Search;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -23,34 +25,19 @@ public class FindFile {
         return result;
     }
 
-    public static void validate(String[] args) {
-        if (args.length != 4) {
-            throw new IllegalArgumentException(
-                    String.format("введено некорректное количество аргументов: %d", args.length));
-        }
-
-        Path path = Paths.get(args[0]);
+    private static void validate(String input, String filterType) {
+        Path path = Paths.get(input);
         if (!Files.exists(path) || !path.toFile().isDirectory()) {
-            throw new IllegalArgumentException(String.format("введен неправильный путь: %s", args[0]));
+            throw new IllegalArgumentException(String.format("введен неправильный путь: %s", input));
         }
 
-        if (!("name".equals(args[2]) || "mask".equals(args[2]) || "regex".equals(args[2]))) {
-            throw new IllegalArgumentException(String.format("введен неправильный фильтр: %s", args[2]));
+        if (!("name".equals(filterType) || "mask".equals(filterType) || "regex".equals(filterType))) {
+            throw new IllegalArgumentException(String.format("введен неправильный фильтр: %s", filterType));
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        String input = args[0].replace("-d=", "");
-        String filter = args[1].replace("-n=", "");
-        String filterType = args[2].replace("-t=", "");
-        String output = args[3].replace("-o=", "");
+    private static List<String> choose(String filterType, List<String> files, String filter) {
         List<String> result = new ArrayList<>();
-
-        validate(args);
-        Path start = Paths.get(input);
-        Visitor visitor = new Visitor();
-        Files.walkFileTree(start, visitor);
-        List<String> files = visitor.getList();
 
         if ("name".equals(filterType)) {
             for (String file : files) {
@@ -61,6 +48,7 @@ public class FindFile {
         }
 
         if ("mask".equals(filterType)) {
+            filter = filter.replace(".", "\\.");
             filter = filter.replace("*", "\\w*");
             filter = filter.replace("?", "\\w");
             result = regex(filter, files);
@@ -69,10 +57,38 @@ public class FindFile {
         if ("regex".equals(filterType)) {
             result = regex(filter, files);
         }
+        return result;
+    }
 
+    private static void write(List<String> result, String output) throws IOException {
         PrintWriter out = new PrintWriter(output);
         out.print(result);
         out.close();
-        System.out.println("see: " + args[3]);
+        System.out.println("see: " + output);
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Path> files;
+        List<String> result = new ArrayList<>();
+
+        if (args.length != 4) {
+            throw new IllegalArgumentException(
+                    String.format("введено некорректное количество аргументов: %d", args.length));
+        }
+        ArgsName argsName = ArgsName.of(args);
+        String input = argsName.get("d");
+        String filter = argsName.get("n");
+        String filterType = argsName.get("t");
+        String output = argsName.get("o");
+        validate(input, filterType);
+
+        Path start = Paths.get(input);
+         files = Search.search(start, p -> p.toFile().isFile());
+        for (Path path : files) {
+            result.add(path.getFileName().toString());
+        }
+
+        result = choose(filterType, result, filter);
+        write(result, output);
     }
 }
