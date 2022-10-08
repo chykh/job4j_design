212fd4a35567adc4196c58d47ca9d43c75ff60b9
chykh <chykh@mail.ru>
2022-10-08 18:25:18 +0300

    Вспомогательный

--- sql/diary-insert1.sql
+++
@@ -1,7 +0,0 @@
-insert into lessons (name) values ('geography');
-insert into lessons (name) values ('russian langauge');
-insert into lessons (name) values ('math');
-insert into lessons (name) values ('history');
-insert into lessons (name) values ('sport');
-insert into lessons (name) values ('singing');
-insert into lessons (name) values ('drawing');
