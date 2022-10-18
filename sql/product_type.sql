59a471b13cf411631cfd7699f3a2206b03f3e835
chykh <chykh@mail.ru>
2022-10-18 21:48:40 +0300

    3. Фильтры [#1732]

--- sql/product_type.sql
+++ sql/product_type.sql
@@ -23,11 +23,41 @@ insert into product (name, type_id, expired_date, price) values ('Лаваш', 3
 insert into product (name, type_id, expired_date, price) values ('Сникерс', 4, '2023-03-13', 47);
 insert into product (name, type_id, expired_date, price) values ('Маасдам', 5, '2023-04-23', 550);
 
-select * from product join type on product.type_id = type.id where type.name = 'Сыр';
-select * from product where product.name like '%мороженое%';
-select * from product where product.expired_date < CURRENT_DATE;
-select * from product order by price desc limit 1;
-select type.name, count(*) from product join type on product.type_id = type.id group by type.name;
-select * from product join type on product.type_id = type.id where type.name = 'Сыр' or type.name = 'Молоко';
-select type.name from product join type on product.type_id = type.id group by type.name  having count(*) < 10;
-select * from product join type on product.type_id = type.id;
+select * from product as p
+join type as t 
+on p.type_id = t.id
+where t.name = 'Сыр';
+
+select * from product 
+where product.name like '%мороженое%';
+
+select * from product
+where product.expired_date < CURRENT_DATE;
+
+select max(product.price) 
+from product;
+
+select * from product
+where product.price = 600;
+
+select t.name, count(*) 
+from product as p join type as t 
+on p.type_id = t.id
+group by t.name;
+
+select * from product as p
+join type as t
+on p.type_id = t.id
+where t.name = 'Сыр'
+or t.name = 'Молоко';
+
+select t.name
+from product as p
+join type as t 
+on p.type_id = t.id
+group by t.name 
+having count(*) < 10;
+
+select * from product as p
+join type as t
+on p.type_id = t.id;
